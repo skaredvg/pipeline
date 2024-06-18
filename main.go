ee65d1f45c265ba8ffcca18960e0eca23cb0229a
skaredvg72 <skaredvg72@gmail.com>
2024-06-18 22:01:42 +0300

    Добавлено логгирование

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 	"strconv"
 	"strings"
@@ -35,10 +36,10 @@ func NewRingBuffer(size uint, timeout uint) *RingBuffer {
 
 // метод вставки элемента в буфер
 func (r *RingBuffer) Set(n int) {
-	fmt.Println("Set")
+	f := "function Set:"
 	r.mux.Lock()
-	fmt.Println("Set begin: r.sidx=", r.sidx, " r.gidx=", r.gidx)
-	fmt.Println("r.buf[r.sidx]=", r.buf[r.sidx], " замещен n=", n)
+	log.Printf("%s begin: r.sidx=%s  r.gidx=%s", f, r.sidx, r.gidx)
+	log.Printf("%s r.buf[r.sidx]=%s замещен n=%s", f, r.buf[r.sidx], n)
 	r.buf[r.sidx] = n
 	if r.gidx == r.sidx && !r.buf_empty {
 		r.gidx++
@@ -52,18 +53,20 @@ func (r *RingBuffer) Set(n int) {
 		r.sidx = 0
 	}
 	r.buf_empty = false
-	fmt.Println("Set end: r.sidx=", r.sidx, " r.gidx=", r.gidx)
+	log.Println("%s end: r.sidx=%d r.gidx=%d", f, r.sidx, r.gidx)
 	r.mux.Unlock()
 }
 
 // Метод считывания элемента из буфера
 func (r *RingBuffer) Get() (int, bool) {
+	f := "function Get:"
 	if r.buf_empty {
+		log.Printf("%s buf_empty=%v", f, r.buf_empty)
 		return 0, false
 	}
 	r.mux.Lock()
 	e := r.buf[r.gidx]
-	fmt.Println("Get begin: r.sidx=", r.sidx, " r.gidx=", r.gidx, " e=", e)
+	log.Printf("%s begin: r.sidx=%d  r.gidx=%d e=%v", f, r.sidx, r.gidx, e)
 	r.gidx++
 	if r.gidx >= len(r.buf) {
 		r.gidx = 0
@@ -72,45 +75,48 @@ func (r *RingBuffer) Get() (int, bool) {
 	if r.gidx == r.sidx {
 		r.buf_empty = true
 	}
-	fmt.Println("Get end: r.sidx=", r.sidx, " r.gidx=", r.gidx, " e=", e)
+	log.Printf("%s end: r.sidx=%d r.gidx=%d e=%v", f, r.sidx, r.gidx, e)
 	r.mux.Unlock()
 	return e, true
 }
 
 // заполяем кольцевой буфер из внешнего источника (канала)
 func (r *RingBuffer) WriteToBuffer(ch <-chan int) <-chan any {
+	f := "function WriteToBuffer"
+	log.Printf("%s Begin")
 	done := make(chan any)
 	go func(ch <-chan int) {
 		defer close(done)
 		for n := range ch {
-			fmt.Println("WriteToBuffer Обрабатываю ", n)
+			log.Printf("%s executing %v", f, n)
 			//<-time.After(r.timeout)
 			r.Set(n)
 		}
-		fmt.Println("Вышел WriteToBuffer")
+		log.Printf("%s End")
 	}(ch)
 	return done
 }
 
 // Читаем из кольцевого буфера во внешний приемник(канал)
 func (r *RingBuffer) ReadFromBuffer(done <-chan any) <-chan int {
+	f := "function ReadFromBuffer"
+	log.Printf("%s Begin", f)
 	c := make(chan int)
 	go func() {
 		defer func() {
-			fmt.Println("Вышел ReadFromBuffer")
+			log.Printf("%s End")
 			close(c)
 		}()
 		for {
 			<-time.After(r.timeout)
-			//			fmt.Println("ReadFromBuffer перед чтением буфера!")
 			select {
 			case <-done:
 				return
 			default:
 				if n, b := r.Get(); b {
-					fmt.Println("ReadFromBuffer прочитал из буфера ", n, b)
+					log.Printf("%s n=%v b=%v", f, n, b)
 					c <- n
-					fmt.Println("ReadFromBuffer отправил в канал печати ", n)
+					log.Printf("%s send to channel printing n=%v", f, n)
 				}
 			}
 		}
@@ -121,68 +127,77 @@ func (r *RingBuffer) ReadFromBuffer(done <-chan any) <-chan int {
 
 // фильтруем числа
 func filterNumber(cs <-chan string) <-chan int {
+	f := "function filterNumber"
+	log.Printf("%s Begin", f)
 	c := make(chan int)
 	go func(cs <-chan string) {
 		defer close(c)
 		for s := range cs {
 			i, err := strconv.Atoi(s)
 			if err == nil {
-				fmt.Println("filterNumber Обрабатываю ", i)
+				log.Printf("%s executing i=%v", f, i)
 				c <- i
 			}
 		}
-		fmt.Println("Вышел из filterNumber")
+		log.Printf("%s End")
 	}(cs)
 	return c
 }
 
 // фильтруем положительные числа
 func filterPositiveNumber(cn <-chan int) <-chan int {
+	f := "function filterPositiveNumber"
 	c := make(chan int)
+	log.Printf("%s Begin", f)
 	go func(cn <-chan int) {
 		defer close(c)
 		for n := range cn {
 			if n >= 0 {
-				fmt.Println("filterPositiveNumber Обрабатываю ", n)
+				log.Printf("%s executing n=", f, n)
 				c <- n
 			}
 		}
-		fmt.Println("Вышел из filterPositiveNumber")
+		log.Printf("%s", f)
 	}(cn)
 	return c
 }
 
 // фильтруем числа больше 0 и кратные 3
 func filterDiv3(cn <-chan int) <-chan int {
+	f := "function filterDiv3"
 	c := make(chan int)
+	log.Printf("%s Begin", f)
 	go func(cn <-chan int) {
 		defer close(c)
 		for n := range cn {
 			if n > 0 && n%3 == 0 {
-				fmt.Println("filterDiv3 Обрабатываю ", n)
+				log.Printf("%s excuting %v", f, n)
 				c <- n
 			}
 		}
-		fmt.Println("Вышел из filterDiv3")
+		log.Printf("%s End", f)
 	}(cn)
 	return c
 }
 
 // вывод в консоль инфорамции
 func OutputToConsole(ch <-chan int) <-chan any {
+	f := "OutputToConsole"
 	done := make(chan any)
+	log.Printf("%s Begin", f)
 	go func(ch <-chan int) {
 		defer close(done)
 		for n := range ch {
-			fmt.Println("Получены данные ... ", n)
+			log.Printf("%s receiving data n=%v", f, n)
 		}
-		fmt.Println("Вышел OutputToConsole")
+		log.Printf("%s End")
 	}(ch)
 	return done
 }
 
 // ввод данных из источника- консоли
 func InputFromConsole() <-chan string {
+	f := "function InputFromConsole"
 	s := bufio.NewScanner(os.Stdin)
 	c := make(chan string)
 	fmt.Println("Вводите числа через пробел")
@@ -196,26 +211,30 @@ func InputFromConsole() <-chan string {
 			}
 			ts := strings.Split(s.Text(), " ")
 			for _, v := range ts {
-				fmt.Println("InputFromConsole Обрабатываю ", v)
+				log.Printf("%s excuting v=%v", f, v)
 				c <- v
 			}
 		}
-		fmt.Println("Вышел из InputFromConsole")
+		log.Printf("%s End", f)
 	}()
 	return c
 }
 
 func Run(size uint, to uint) {
+	f := "function Run"
+	log.Printf("%s Begin", f)
 	buf := NewRingBuffer(size, to)
 	c := filterDiv3(filterPositiveNumber(filterNumber(InputFromConsole())))
 	done := buf.WriteToBuffer(c)
 	c2 := buf.ReadFromBuffer(done)
 	done2 := OutputToConsole(c2)
 	<-done2
-	fmt.Println("Вышел из Run")
+	fmt.Println("%s End", f)
 }
 
 func main() {
+	f := "function main"
+	log.Printf("%s Begin", f)
 	Run(sizeBuffer, timeOutReadBuffer)
-	fmt.Println("Вышел из main")
+	log.Printf("%s End", f)
 }
